40f2f2876385529244ff2c1f6370cadf3229c435
Alejandro Lopez <alejozepol@gmail.com>
2019-04-14 16:39:10 -0500

    servicios de logeo

--- firstPips/src/app/app.module.ts
+++ firstPips/src/app/app.module.ts
@@ -3,6 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AutorizacionSericios } from './servicios/autorizacion.servicios';
+
+
+import {AngularFireModule} from "@angular/fire"
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+
+const  firebaseConfig = {
+  apiKey: "AIzaSyDCuSSpHi3L9PSbLnCOGid3zuQLqSSwNj0",
+  authDomain: "fistpips.firebaseapp.com",
+  databaseURL: "https://fistpips.firebaseio.com",
+  projectId: "fistpips",
+  storageBucket: "",
+  messagingSenderId: "823849314972"}
 
 @NgModule({
   declarations: [
@@ -10,9 +26,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule,
+        // import HttpClientModule after BrowserModule.
   ],
-  providers: [],
+  providers: [AutorizacionSericios],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ firstPips/src/app/servicios/autorizacion.servicios.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
+
+@Injectable()
+
+export class AutorizacionSericios{
+  constructor(//afDB  objeto para conexion con base de datos de firebase
+    private afDB: AngularFireDatabase,
+    // angularFireAuth objeto para autenticacion con las opciones de firebase
+    private angularFireAuth: AngularFireAuth, private router:Router){
+      this.islogged()
+  }
+  public login = (email, clave) =>{
+    this.angularFireAuth.auth.signInWithEmailAndPassword(email, clave)
+                      .then((respuesta)=>{
+                        alert('Usuario logueado')
+                        this.router.navigate(['lugares'])
+                      })
+                    .catch((error)=>{
+                      alert('un error a ocurrido')
+                      console.log(error)
+                    })
+  }
+
+  public logout(){
+    // metodo para logueo con usuario y contraseña
+    return this.angularFireAuth.auth.signOut();
+  }
+
+  public registro = (email , clave) =>{
+
+    this.angularFireAuth.auth.createUserWithEmailAndPassword(email, clave)
+                      .then((respuesta)=>{
+                        alert('Usuario registrado con exito')
+                        this.router.navigate(['lugares'])
+
+                      })
+                    .catch((error)=>{
+                      alert('un error a ocurrido')
+                      console.log(error)
+                    })
+                  }
+
+  public islogged(){
+    return this.angularFireAuth.authState;
+  }
+
+  public getUsuario(){
+    return this.angularFireAuth.auth
+  }
+
+  loginFacebook(){
+    //metodo de logueo con facebook utilizando un popup para ingreso a la red social y devolverse a la pagina de la aplicacion
+      this.angularFireAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+          .then((resultado)=>{
+            console.log(resultado)
+              alert('Usuario logueado con facebook')
+              this.router.navigate(['lugares'])
+          })
+          .catch((error)=>{
+            console.log(error)
+          })
+  }
+
+  loginGoogle(){
+    this.angularFireAuth.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider())
+      .then((resultado)=>{
+        console.log(resultado)
+          alert('Usuario logueado con Google')
+          this.router.navigate(['lugares'])
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+  }
+}
