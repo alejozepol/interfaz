ccb84644d4a6657381138b4354a747911bd47579
Alejandro Lopez <alejozepol@gmail.com>
2019-04-24 02:44:51 -0500

    arreglo formulario de logueo fontend

--- firstPips/src/app/logueo/logueo.component.ts
+++ firstPips/src/app/logueo/logueo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AutorizacionSericios } from '../servicios/autorizacion.servicios';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-logueo',
@@ -11,6 +12,20 @@ export class LogueoComponent {
   loginP : any ={}
   constructor( private autorizacionService: AutorizacionSericios) {
    }
+
+   formLogin = new FormGroup({
+    email             : new FormControl('',[Validators.required, Validators.email]),
+    clave             : new FormControl('',[Validators.required, Validators.minLength(6)])
+   })
+
+errorCorreo() {
+  return this.formLogin.controls.email.hasError('required') ? 'Correo electronico es requerido' :
+  this.formLogin.controls.email.hasError('email') ? 'Correo electronico no Valido' : '';
+     }
+errorClave() {
+  return this.formLogin.controls.clave.hasError('required') ? 'La contraseña es requerido' :
+  this.formLogin.controls.clave.hasError('minLength') ? 'La contraseña no Valido' : 'La contraseña no Valido';
+  }
 
    login(){
         this.autorizacionService.login(  this.loginP.email,this.loginP.clave)
