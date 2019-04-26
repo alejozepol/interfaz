1f94804da421709e4d0c682e4356301b61896262
Alejandro Lopez <alejozepol@gmail.com>
2019-04-26 00:39:06 -0500

    insercion de grafica

--- firstPips/src/app/deshboard/deshboard.componentes.ts
+++ firstPips/src/app/deshboard/deshboard.componentes.ts
@@ -2,19 +2,19 @@ import { Component } from "@angular/core";
 import { AutorizacionSericios } from '../servicios/autorizacion.servicios';
 import { SenalesServicios } from '../servicios/senales.servicios';
 import { Router } from '@angular/router';
-
-
+declare const TradingView: any;
 @Component({
   selector: 'app-root',
   templateUrl: '../deshboard/deshboard.componentes.html',
 })
+
 
 export class DeshboardComponentes{
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
    senales: any = []
    usuarios = null
-   senal = {}
+   senal: any = {}
    vista = 0
 
   constructor(private senalesServicio: SenalesServicios,
@@ -22,6 +22,8 @@ export class DeshboardComponentes{
 
               senalesServicio.getSenales().valueChanges()
                             .subscribe(senales => this.senales = senales)
+
+
   }
 
 consularSenal(id){
@@ -31,8 +33,35 @@ consularSenal(id){
           if (e.id === id) {
             this.senal=e
           }
-        });
+
+        })
+
+        new TradingView.widget({
+          "width": 400,
+          "symbol": `OANDA:${this.senal.grupoDivisa}`,
+          "interval": "120",
+          "timezone": "Etc/UTC",
+          "theme": "Dark",
+          "style": "1",
+          "locale": "es",
+          "toolbar_bg": "#f1f3f6",
+          "enable_publishing": false,
+          "hide_top_toolbar": true,
+          "allow_symbol_change": true,
+          "container_id": "graficaTW",
+          'withdateranges': true,
+          'hide_side_toolbar': false,
+          'save_image': false,
+          'hideideas': true,
+          'studies': [
+          'MASimple@tv-basicstudies' ],
+          'show_popup_button': true,
+          'popup_width': '1000',
+          'popup_height': '650'
+        })
   }
+
+
   anterior(){
   this.vista = 0
 }
--- firstPips/src/app/detalleSenal/detalleSenal.component.ts
+++ firstPips/src/app/detalleSenal/detalleSenal.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SenalesServicios } from '../servicios/senales.servicios';
 import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import tw from './tv'
 
 
+declare const TradingView: any;
 @Component({
   selector: 'app-detalle-senal',
   templateUrl: './detalleSenal.component.html',
@@ -16,21 +15,11 @@ export class DetalleSenalComponent {
   id = null
   constructor(private senalesServicio: SenalesServicios,
     private activatedRoute: ActivatedRoute, private router:Router) {
+
     this.id = this.activatedRoute.snapshot.params['id']
-      if( this.id != 'new'){
-    this.senal = this.senalesServicio.getSenal(this.id).valueChanges()
-        .subscribe( senal => {this.senal = senal
-          console.log(senal)
-          console.log(this.senal)
-          return senal
-
-        } )
-
-        console.log(this.senal)
 
 
     }
 
 
     }
-}
